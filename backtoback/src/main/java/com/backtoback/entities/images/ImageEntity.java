2a5c414d4fbce3be3c1c7f37a78b966270b2fb4e
Juan Pablo Amador <juampablo13@gmail.com>
2015-06-13 10:18:38 -0600

    Added initial entities OfyHelper to persist data

---
+++ backtoback/src/main/java/com/backtoback/entities/images/ImageEntity.java
@@ -0,0 +1,41 @@
+package com.backtoback.entities.images;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+@Entity
+public class ImageEntity {
+	@Id
+	private Long id;
+	private BlobKey blobKey;
+	private String servingURL;
+	@Index
+	private String eventId;
+
+	public BlobKey getBlobKey() {
+		return blobKey;
+	}
+
+	public void setBlobKey(BlobKey blobKey) {
+		this.blobKey = blobKey;
+	}
+
+	public String getServingURL() {
+		return servingURL;
+	}
+
+	public void setServingURL(String servingURL) {
+		this.servingURL = servingURL;
+	}
+
+	public String getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(String eventId) {
+		this.eventId = eventId;
+	}
+
+}
---
+++ backtoback/src/main/java/com/backtoback/entities/products/ProductEntity.java
@@ -0,0 +1,56 @@
+package com.backtoback.entities.products;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+@Entity
+public class ProductEntity {
+
+	@Id
+	private Long id;
+	private String name;
+	private String brand;
+	private String productURL;
+	private String photoURL;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getProductURL() {
+		return productURL;
+	}
+
+	public void setProductURL(String productURL) {
+		this.productURL = productURL;
+	}
+
+	public String getPhotoURL() {
+		return photoURL;
+	}
+
+	public void setPhotoURL(String photoURL) {
+		this.photoURL = photoURL;
+	}
+
+}
